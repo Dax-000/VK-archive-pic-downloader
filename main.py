a4fe887729fa677fbf68f3b482e650f18af7b6e0
Dax-000 <CiderBinderBunny@yandex.ru>
2024-03-30 15:56:22 +0500

    Добавить извлечение даты и её форматирование

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ input_path = 'input'
 output_path = 'output'
 
 
+def date_fmt(_date):
+    "Серафим Казачков, 19 авг 2023 в 7-32-44"
+    _spt1 = _date.split(', ')
+    _name = _spt1[0]
+    _spt_date = _spt1[1].split(' ')
+    _new_date = f"{_spt_date[2]} {_spt_date[1]} {_spt_date[0]} {_spt_date[-1]} ({_name})"
+    return _new_date
+
+
 def get_html_list():
     _html_list = set()
     _htmls = os.listdir(input_path)
@@ -21,10 +30,13 @@ def get_links(_file_path):
     _got_links = set()
     with open(_file_path, 'r') as html:
         soup = BeautifulSoup(html, "html.parser")
-        att_links = soup.findAll('a', class_="attachment__link")
+        att_links = soup.findAll('div', class_="message")
+        att_links = [link for link in att_links if link.find('a', class_="attachment__link")]
         for link in att_links:
-            if link['href'][8:11] == "sun":
-                _got_links.add(link['href'])
+            _href = str(link.find('a', class_="attachment__link")['href'])
+            if _href[8:11] == "sun":
+                _date = date_fmt(link.find('div', class_="message__header").text)
+                _got_links.add((_href, _date))
     return _got_links
 
 
@@ -43,10 +55,9 @@ def get_all_links():
         _all_links.update(get_links(html))
     with open('links.txt', 'w') as f:
         for link in _all_links:
-            f.write(str(link)+'\n')
+            f.write(link[0]+', '+link[1]+'\n')
 
 
 if __name__ == "__main__":
-    pass
-    # get_all_links()
+    get_all_links()
     #download_links()
